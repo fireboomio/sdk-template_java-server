711564cbbeaea6d05dfbcc3983d5d40b504ac99f
xufeixiang <xufeixiang@ainanjing.org.cn>
2023-10-01 11:53:05 +0800

    feat: ignore unimportant request log.

--- files/src/main/java/io/fireboom/plugins/RequestLoggingInterceptor.java
+++ files/src/main/java/io/fireboom/plugins/RequestLoggingInterceptor.java
@@ -1,24 +1,33 @@
 package io.fireboom.plugins;
 
+import io.fireboom.server.enums.Endpoint;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Slf4j
 public class RequestLoggingInterceptor implements HandlerInterceptor {
     private long startTime;
+    AntPathMatcher pathMatcher = new AntPathMatcher();
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         startTime = System.currentTimeMillis();
         return true;
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        String requestURI = request.getRequestURI();
+        if (Arrays.stream(Endpoint.values()).noneMatch(x -> pathMatcher.match(x.getValue(), requestURI))) {
+            return;
+        }
+
         long duration = System.currentTimeMillis() - startTime;
-        log.info("request url: {}, cost: {}ms, status: {}, exception: {}", request.getRequestURI(), duration, response.getStatus(), ex);
+        log.info("request url: {}, cost: {}ms, status: {}, exception: {}", requestURI, duration, response.getStatus(), ex);
     }
 }
